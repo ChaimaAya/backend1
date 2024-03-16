d8dc6dd689f6e7e08373103fb84a71df118f6627
aya <ayahmida64@gmail.com>
2024-03-16 20:07:53 +0100

    publications created

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PublicationsController;
 use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -28,5 +29,15 @@ Route::group(['middleware'=>'api','prefix'=>'auth'],function($router){
     Route::get('/startup',[AuthController::class,'getStartupDetailsForUser']);
     Route::post('/logout',[AuthController::class,'logout']);
     Route::post('/getUserType',[AuthController::class,'getUserType']);
+
+    Route::get('/publications', [PublicationsController::class, 'index']);
+    Route::post('/publication', [PublicationsController::class, 'store']);
+    Route::get('/publicationsUser', [PublicationsController::class, 'userProfilePublications']);
+
+
+Route::get('/publications/{id}', [PublicationsController::class, 'show']);
+Route::get('/publications/{id}/edit', [PublicationsController::class, 'edit']);
+Route::put('/publications/{id}/edit', [PublicationsController::class, 'update']);
+Route::delete('/publications/{id}/', [PublicationsController::class, 'destroy']);
 
 });
